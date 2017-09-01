82542b9cc78aea0b711851f8dac08974ec387d7c
AffeAli <affe-ali@web.de>
2017-09-01 12:07:23 +0200

    index redesign

--- js/IndexPage.js
+++ js/IndexPage.js
@@ -2,13 +2,6 @@ class IndexPage extends ZeroFrame {
 
     fillList() {
         this.cmd("dbQuery", ["SELECT * FROM topic LEFT JOIN json USING(json_id) ORDER BY added DESC"], (topics) => {
-            /*var html = "<ul class=\"project-list\">"
-            for(var i = 0; i < topics.length; i++) {
-                html += "<li class=\"project-list-item\"><a class=\"project-link\" href=\"mod_entry.html?mod_address=" + topics[i].topic_id + topics[i].directory.replace("users/", "_") + "\">" + topics[i].name + "</a>" + "<img class=\"list-category-item\" src=\"icons/cat_" + topics[i].category + ".png\">"
-                html += "</li>"
-            }
-            html += "</ul>"
-            document.getElementById("new_mod_list").innerHTML = html*/
             var list = document.createElement("ul")
             list.className = "project-list"
             for(var i = 0; i < topics.length; i++) {
@@ -32,37 +25,27 @@ class IndexPage extends ZeroFrame {
                         mod_link.innerHTML = topics[i].name
                         div_details.append(mod_link)
                         
+                        var creator_link = document.createElement("a")
+                        creator_link.className = "nolink creator"
+                        creator_link.href = "modder_profile.html?auth_address=" + topics[i].directory.replace("users/", "")
+                        creator_link.innerHTML = " by " + topics[i].cert_user_id
+                        div_details.append(creator_link)
+                        
                         var category_icon = document.createElement("img")
                         category_icon.className = "list-category-icon"
+                        category_icon.style = "display: none;"
                         category_icon.src = "icons/cat_" + topics[i].category + ".png"
                         div_details.append(category_icon)
+                        
+                        var summary_div = document.createElement("div")
+                        summary_div.style = "color: black; margin-top: 20px;"
+                        summary_div.innerHTML = topics[i].summary
+                        div_details.append(summary_div)
                     item.append(div_details)
                 list.append(item)
             }
             $("new_mod_list").append(list)
         })
-    }
-
-    newMod() {
-        if(!this.site_info.cert_user_id) {
-            this.cmd("wrapperNotification", ["info", "Please, select your account."])
-            return false
-        }
-        var data_path = "data/users/" + this.site_info.auth_address + "/data.json"
-        this.cmd("fileGet", { "inner_path": data_path, "required": false }, (file) => {
-            if(file) {
-                //TODO fix link  window.location.href = "mod_entry.html?mod_address=99_" + this.site_info.auth_address
-                //TODO Check if valid
-            }
-            else {
-                if(confirm("To create a new mod, you need to create a modder profile first\nDo you want to create one?") == true)
-                    window.location.href = "modder_profile.html?auth_address=" + this.site_info.auth_address
-                else
-                    alert("mod creation cancelled!")
-            }
-        })
-        
-        return false
     }
     
     ownPage() {
